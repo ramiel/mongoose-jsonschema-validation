0b5e9649b1cd587d48ce1d623df02c4202633837
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2015-05-29 14:58:21 +0200

    Added test environment

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,13 +13,45 @@ module.exports = function(grunt) {
       lib_test: {
         src: ['lib/**/*.js', 'test/**/*.js']
       }
-    }
+    },
+    mochacov: {
+      test: {
+        options: {
+          reporter: 'spec'
+        }
+      },
+      coverage: {
+        options: {
+          coveralls: {
+            repoToken: '0aRlqM9q5jCD6L0Gnjx0sxmNHT5dI23aC'
+          }
+        }
+      },
+      local_coverage: {
+        options: {
+          reporter: 'html-cov',
+          quiet: true,
+          output: 'coverage/coverage.html'
+        }
+      },
+      options: {
+        files:{
+          src: ['test/**/*.js','!test/fixtures','!test/helpers']
+        }
+      }
+    },
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-cov');
 
   // Default task.
   grunt.registerTask('default', ['jshint']);
+  if(process.env.TRAVIS){
+    grunt.registerTask('test', ['mochacov:test', 'mochacov:coverage']);
+  }else{
+    grunt.registerTask('test', ['mochacov:test', 'mochacov:local_coverage']);
+  }
 
 };
